6d83cf016a25d441621262c119b876c46d48ef4b
ahmed al-hanini <a.hanini111@gmail.com>
2024-07-06 21:03:36 +0300

    Profile

--- lib/pages/settings_pages/settings_page.dart
+++ lib/pages/settings_pages/settings_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:testing/components/languages_dialog.dart';
 import 'package:testing/components/my_app_bar.dart';
 import 'package:testing/components/my_settings_button.dart';
+import 'package:testing/constants/app_colors.dart';
+import 'package:testing/constants/app_sizes.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -20,6 +22,26 @@ class SettingsPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SizedBox(
+              height: Sizes.width(context) / 3,
+              width: Sizes.width(context) / 3,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXJA32WU4rBpx7maglqeEtt3ot1tPIRWptxA&s',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Text(
+              'Name Of User',
+              style: TextStyle(
+                color: AppColors.black,
+                fontSize: Sizes.allSizes(context) / 75,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 100),
             MySettingsButton(
               onTap: () {
                 languagesDialog(context);
